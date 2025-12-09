71a97642c29d544961c8bfff147b6c5bce1b54d0
Mykal-Steele <mykal.stele@gmail.com>
2025-12-09 22:32:32 +0700

    feat: add struct for movie, genre and ranking

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+func main() {
+	router:=gin.Default()
+	router.GET("/hello", func(c *gin.Context){
+		c.String(200, "Hello, MagicStreamMovies")
+	})
+	if err:=router.Run(":8080"); err != nil{
+		fmt.Println("There is an error: ", err)
+	}
+}
