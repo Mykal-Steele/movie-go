89cbb2ce59c52e9c57eefc9205047692385db527
Mykal-Steele <mykal.stele@gmail.com>
2025-12-09 22:35:48 +0700

    feat: call controller w/ routes

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 
+	controller "github.com/Mykal-Steele/movie-go/Server/MagicStreamMoviesServer/controllers"
 	"github.com/gin-gonic/gin"
 )
+
 func main() {
-	router:=gin.Default()
-	router.GET("/hello", func(c *gin.Context){
-		c.String(200, "Hello, MagicStreamMovies")
-	})
-	if err:=router.Run(":8080"); err != nil{
+	router := gin.Default()
+	router.GET("/hello", controller.GetHello())
+
+	router.GET("/movies", controller.GetMovies())
+	router.GET("/genres", controller.GetGenre())
+	if err := router.Run(":8080"); err != nil {
 		fmt.Println("There is an error: ", err)
 	}
 }
